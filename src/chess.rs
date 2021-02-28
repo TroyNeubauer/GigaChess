2b9dffc1f9c13a3d483e4fef8f56da0fb33a188e
Troy Neubauer <troyneubauer@gmail.com>
2021-02-28 10:45:28 -0500

    Squares and pieces now generically depend on the board type

--- src/chess.rs
+++ src/chess.rs
@@ -1,60 +1,115 @@
 use crate::chess_like::*;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum ChessPiece {
-    KING,
-    QUEEN,
-    ROOK,
-    BISHOP,
-    KNIGHT,
-    PAWN,
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 struct ChessBoard {
-    board: [[ChessPiece; 8]; 8],
+    board: [RawSquare<ChessPiece, DefaultColorScheme>; 64],
 }
 
-struct LegalChessMovesIterator {
+struct RawMoveIterator {
     board: ChessBoard,
-    last_move: Option<Move>,
+    last_move: Option<Move<ChessBoard>>,
 }
 
-impl GenericBoard<ChessPiece, ChessBoard, LegalChessMovesIterator> for ChessBoard {
-    fn is_move_legal(&self, board_move: Move) -> bool {
-        false
+impl GenericBoard for ChessBoard {
+    type PieceType = ChessPiece;
+    type ColorType = DefaultColorScheme;
+    type RawMoveIteratorType = RawMoveIterator;
+
+    fn side_len() -> u8 {
+        8
     }
 
-    fn raw_moves_for_piece(&self, pos: SquarePos) -> LegalChessMovesIterator {
-        LegalChessMovesIterator {
+    fn new() -> ChessBoard {
+        ChessBoard {
+            board: [RawSquare::empty(); 64],
+        }
+    }
+
+    fn default() -> ChessBoard {
+        let mut result = ChessBoard::new();
+
+        result
+    }
+
+    fn raw_moves_for_piece(&self, pos: SquarePos<ChessBoard>) -> RawMoveIterator {
+        RawMoveIterator {
             board: self.clone(),
             last_move: None,
         }
     }
 
-    fn get_attackers_of_square(&self, target_pos: SquarePos) -> Vec<SquarePos> {
+    fn get_attackers_of_square(
+        &self,
+        target_pos: SquarePos<ChessBoard>,
+    ) -> Vec<SquarePos<ChessBoard>> {
         let mut result = Vec::new();
-        for pos in self.square_iter() {
+        for pos in self.raw_square_iter() {
             if self.is_move_legal(Move::new(pos, target_pos)) {
                 result.push(pos);
             }
-
         }
         result
     }
 
-    fn square_iter(&self) -> SquareIter {
-        let side_len = self.board.len();
-        let max_size: u8 = (side_len * side_len) as u8;
+    fn raw_square_iter(&self) -> SquareIter<ChessBoard> {
+        let max_size = (ChessBoard::side_len() * ChessBoard::side_len()) as u8;
         SquareIter::new(max_size, 0)
     }
 
+    fn get(&self, pos: SquarePos<ChessBoard>) -> &RawSquare<ChessPiece, DefaultColorScheme> {
+        &self.board[pos.raw_value() as usize]
+    }
+
+    ///Swaps the piece on the board with the mutable piece specified
+    fn swap(
+        &self,
+        pos: SquarePos<ChessBoard>,
+        piece: &mut RawSquare<ChessPiece, DefaultColorScheme>,
+    ) {
+    }
+
+
+    fn is_move_legal(&self, board_move: Move<ChessBoard>) -> bool {
+        let it = self.raw_moves_for_piece(board_move.src);
+        for generated_move in it {
+            if generated_move == board_move {
+                //If we can find a matching generated raw move then we are on the right track.
+                //Now we just need to check for checks and we are good.
+                return true;
+            }
+        }
+
+        false
+    }
+ 
+
+
 }
 
-impl Iterator for LegalChessMovesIterator {
-    type Item = Move;
+impl Iterator for RawMoveIterator {
+    type Item = Move<ChessBoard>;
 
     fn next(&mut self) -> Option<Self::Item> {
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn legal_moves_it1() {
+        let board: ChessBoard = ChessBoard::new();
     }
 }
--- src/chess_like.rs
+++ src/chess_like.rs
@@ -1,58 +1,100 @@
-pub trait GenericBoard<PieceType, BoardType, IteratorType: Iterator<Item = Move>> {
-    fn is_move_legal(&self, board_move: Move) -> bool {
-        let it = self.raw_moves_for_piece(board_move.src);
-        for generated_move in it {
-            if generated_move == board_move {
-                //If we can find a matching generated raw move then we are on the right track.
-                //Now we just need to check for checks and we are good.
-                return true;
-            }
-        }
+use std::marker::PhantomData;
 
-        false
-    }
-    ///Enumerates the 'raw' moves using the movement rules for the piece occupying the requested
-    ///square. Raw means the list may contain moves that transitively are illegal because they
-    ///cause checks.
-    fn raw_moves_for_piece(&self, pos: SquarePos) -> IteratorType;
-
-    ///Returns a list of the locations of the pieces that attack a square. Attacking is defined as
-    ///having a legal move that moves takes a potential attacker its starting position to pos
-    fn get_attackers_of_square(&self, target_pos: SquarePos) -> Vec<SquarePos>;
-
-    fn square_iter(&self) -> SquareIter;
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum DefaultColorScheme {
+    While,
+    Black,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub struct SquarePos {
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct RawSquare<PieceType, ColorType> {
+    pub data: Option<(PieceType, ColorType)>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct SquarePos<BoardType> {
     pos: u8,
+    _board_type: PhantomData<BoardType>,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, new)]
-pub struct Move {
-    src: SquarePos,
-    dest: SquarePos,
+#[derive(Copy, Clone, Debug, PartialEq, Eq, new)]
+pub struct Move<BoardType> {
+    pub src: SquarePos<BoardType>,
+    pub dest: SquarePos<BoardType>,
 }
 
-pub trait GenericPiece<Piece, Board, IteratorType: Iterator<Item = Move>> {
+pub trait GenericPiece<Piece, Board, IteratorType: Iterator<Item = Move<Board>>> {
     fn legal_moves_it(board: &Board) -> IteratorType;
 }
 
 #[derive(new)]
-pub struct SquareIter {
+pub struct SquareIter<BoardType> {
     current: u8,
     max_size: u8,
+    _board_type: PhantomData<BoardType>,
 }
 
-impl SquarePos {
-    pub fn from_raw(pos: u8) -> SquarePos {
-        SquarePos { pos }
+pub trait GenericBoard: Sized + PartialEq + Eq {
+    type PieceType: PartialEq + Eq + Copy;
+    type ColorType: PartialEq + Eq + Copy;
+    type RawMoveIteratorType: Iterator<Item = Move<Self>>;
+
+    fn side_len() -> u8;
+    ///Creates an empty board
+    fn new() -> Self;
+
+    ///Creates a board with pieces placed in their default positions
+    fn default() -> Self;
+
+    fn is_move_legal(&self, board_move: Move<Self>) -> bool;
+
+    ///Enumerates the 'raw' moves using the movement rules for the piece occupying the requested
+    ///square. Raw means the list may contain moves that transitively are illegal because they
+    ///cause checks.
+    fn raw_moves_for_piece(&self, pos: SquarePos<Self>) -> Self::RawMoveIteratorType;
+
+    ///Returns a list of the locations of the pieces that attack a square. Attacking is defined as
+    ///having a legal move that moves takes a potential attacker its starting position to pos
+    fn get_attackers_of_square(&self, target_pos: SquarePos<Self>) -> Vec<SquarePos<Self>>;
+
+    fn raw_square_iter(&self) -> SquareIter<Self>;
+
+    fn get(&self, pos: SquarePos<Self>) -> &RawSquare<Self::PieceType, Self::ColorType>;
+
+    ///Swaps the piece on the board with the mutable piece specified
+    fn swap(&self, pos: SquarePos<Self>, piece: &mut RawSquare<Self::PieceType, Self::ColorType>);
+}
+
+impl<BoardType: GenericBoard> SquarePos<BoardType> {
+    pub fn from_raw(pos: u8) -> SquarePos<BoardType> {
+        SquarePos {
+            pos,
+            _board_type: PhantomData::default(),
+        }
+    }
+
+    pub fn new(file: u8, rank: u8) -> SquarePos<BoardType> {
+        SquarePos {
+            pos: file * BoardType::side_len(),
+            _board_type: PhantomData::default(),
+        }
+    }
+
+    pub fn rank(&self) -> u8 {
+        return self.pos % BoardType::side_len();
+    }
+
+    pub fn file(&self) -> u8 {
+        return self.pos / BoardType::side_len();
+    }
+
+    pub fn raw_value(&self) -> u8 {
+        self.pos
     }
 }
 
-
-impl Iterator for SquareIter {
-    type Item = SquarePos;
+impl<BoardType: GenericBoard> Iterator for SquareIter<BoardType> {
+    type Item = SquarePos<BoardType>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.current >= self.max_size {
@@ -63,6 +105,13 @@ impl Iterator for SquareIter {
             Some(SquarePos::from_raw(result))
         }
     }
-
 }
+
+impl<PieceType, ColorType> RawSquare<PieceType, ColorType> {
+    pub fn empty() -> RawSquare<PieceType, ColorType> {
+        RawSquare { data: None }
+    }
+}
+
+
 
