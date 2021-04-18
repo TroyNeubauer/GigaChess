6e13e6acfd8bb52f9edb396761518f28ac8fea3c
Troy Neubauer <troyneubauer@gmail.com>
2021-04-18 17:51:45 -0400

    Switched java jni methods to follow rust naming conventions

--- src/rust_jni.rs
+++ src/rust_jni.rs
@@ -5,11 +5,11 @@ use jni::JNIEnv;
 use jni::objects::{JClass, JString};
 
 #[no_mangle]
-pub extern "system" fn Java_com_troy_chess_Natives_init0(_env: JNIEnv, _class: JClass) {}
+pub extern "system" fn Java_com_troy_chess_Natives_init_1rust(_env: JNIEnv, _class: JClass) {}
 
 
 #[no_mangle]
-pub extern "system" fn Java_com_troy_chess_Natives_startGame(env: JNIEnv, _class: JClass, ja_name: JString, jb_name: JString, game_type_java: JString) {
+pub extern "system" fn Java_com_troy_chess_Natives_start_game(env: JNIEnv, _class: JClass, ja_name: JString, jb_name: JString, game_type_java: JString) {
     let a_name: String = env.get_string(ja_name).expect("Failed to get player a's name!").into();
     let b_name: String = env.get_string(jb_name).expect("Failed to get player b's name!").into();
     let game_type: String = env.get_string(game_type_java).expect("Failed to get the game type").into();
@@ -17,5 +17,4 @@ pub extern "system" fn Java_com_troy_chess_Natives_startGame(env: JNIEnv, _class
     println!("Got game info: {} vs, {}. Type: {}", a_name, b_name, game_type);
 
 }
-
 
